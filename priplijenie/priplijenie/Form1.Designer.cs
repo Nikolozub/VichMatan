bb07ea5311239470341980138c812b5100bfdfae
Nikolozub <51316856+Nikolozub@users.noreply.github.com>
2020-04-08 23:46:37 +0700

    Add files via upload

---
+++ priplijenie/priplijenie/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace priplijenie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.MajorGrid.Enabled = false;
+            chartArea1.AxisY.MajorGrid.Enabled = false;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.LegendText = "Интерполяция";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Legend = "Legend1";
+            series2.LegendText = "Функция";
+            series2.MarkerSize = 10;
+            series2.Name = "Series2";
+            series2.YValuesPerPoint = 2;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.LegendText = "Среднеквадр. откл.\\nСтепень 1";
+            series3.Name = "Series3";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.LegendText = "Среднеквадр. откл.\\nСтепень 2";
+            series4.MarkerBorderColor = System.Drawing.Color.Transparent;
+            series4.Name = "Series4";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.Legend = "Legend1";
+            series5.LegendText = "Среднеквадр. откл.\\nСтепень 3";
+            series5.Name = "Series5";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(675, 328);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(707, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(92, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(707, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(837, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Y";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(837, 34);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(92, 95);
+            this.listBox2.TabIndex = 3;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(713, 291);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(92, 82);
+            this.listBox3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(710, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(193, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Коэфициенты многочлена 1 степени";
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(713, 169);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(92, 82);
+            this.listBox4.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(710, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(193, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Коэфициенты многочлена 2 степени";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 451);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа №3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ priplijenie/priplijenie/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace priplijenie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // интерполяция многочленом в форме лагранжа
+        private double lagrange( double[] X, double[] Y, double x ) 
+        {
+            double y = 0;
+
+            double k;
+            for (int i = 0; i < X.Length; i++)
+            {
+                k = Y[i];
+                for (int j = 0; j < X.Length; j++)
+                {
+                    if (i != j) k *= (x - X[j]) / (X[i] - X[j]);    
+                }
+                y += k;
+            }
+
+            return y;
+        }
+
+        double razd_raznost(double[] X, double[] Y, int i, int n) 
+        {
+            if (n == i) return Y[i];
+            return (razd_raznost(X, Y, i, n-1) - razd_raznost(X, Y, i+1, n)) / (X[i] - X[n]);
+        }
+
+        double newton(double[] X, double[] Y, int k, double x) 
+        {
+            if (k >= X.Length) return 0;
+            return razd_raznost(X, Y, 0, k) + (x - X[k]) * newton(X, Y, k + 1, x);
+        }
+
+        double calc_polynom(double[] A, double x) 
+        {
+            double y = 0;
+            for (int i = A.Length - 1; i >= 0; i--)
+            {
+                y = A[i] + y * x;
+            }
+            return y;
+        }
+
+        // меняет местами столбцы квадратной матрицы
+        void swap_colums(int size, double[,] matrix, int c_1, int c_2)
+        {
+            if ((size <= 0) || (c_1 < 0) || (c_1 >= size) || (c_2 < 0) || (c_2 >= size) || (c_1 == c_2)) return;
+
+            for (int i = 0; i < size; i++) 
+            {
+                double t = matrix[i, c_1];
+                matrix[i, c_1] = matrix[i, c_2];
+                matrix[i, c_2] = t;
+            }
+        }
+
+        // Метод Гаусса с выбором главного элемента по строке 
+        // Возврат результата через параметр X_matrix
+        int gauss( int size, double[,] A_matrix, double[] B_matrix, double[] X_matrix )
+        {
+	        if ( size <= 0 ) return 0;
+
+	        // копируем матрицы
+	        double[,] a_matrix = new double[size,size];
+	        double[]  b_matrix = new double[size];
+	        Array.Copy(A_matrix, a_matrix,  size*size);
+            Array.Copy(B_matrix, b_matrix,  size);
+
+
+	        // порядок следования X
+	        int[] order = new int[size];
+            for (int i = 0; i < size; i++)
+                order[i] = i;
+
+
+	        // формируем треугольную матрицу с перестановкой столбцов
+	        for ( int k = 0; k < size; k++ )
+	        {
+
+		        int max = k;
+		        for ( int i = k + 1; i < size; i++ ) 
+		        {
+			        if ( Math.Abs( a_matrix[k,i] ) > Math.Abs( a_matrix[k,max] ) ) max = i;
+		        }
+
+		        if ( Math.Abs(a_matrix[k,max]) < 0.000001 ) 
+		        {
+			        return 0;
+		        }
+
+		        if (max != k) 
+		        {
+			        swap_colums( size, a_matrix, k, max );
+                    int t = order[k];
+                    order[k] = order[max];
+                    order[max] = t;
+		        }
+
+
+		        for ( int m = k + 1; m < size; m++ )
+		        {
+			        double koef = a_matrix[m, k] / a_matrix[k, k];
+
+			        b_matrix[m] = b_matrix[m] - koef * b_matrix[k];
+
+			        a_matrix[m, k] = 0.0;
+
+			        for ( int l = k + 1; l < size; l++ )
+			        {
+				        a_matrix[m, l] = a_matrix[m, l] - koef * a_matrix[k, l];
+			        }			
+		        }
+				
+	        }
+
+	        // расчитываем X с учетом их порядка следования
+	        for ( int i = size - 1; i >= 0; i-- )
+	        {
+		        double sum = 0;
+		        for ( int j = i + 1; j < size; j++ )
+		        {
+			        sum += a_matrix[i, j] * X_matrix[ order[j] ];
+		        }
+
+		        X_matrix[ order[i] ] = ( b_matrix[i] - sum ) / a_matrix[i, i];
+	        }
+
+	        return 1;
+        }
+
+        double[] naim_kvadrat(double[] X, double[] Y, int k) 
+        {
+            double t;
+            int i, j;
+
+            double[,] C = new double[k + 1, k + 1];
+            double[] D = new double[k + 1];
+
+            // заполняем матрицу C
+            for (int m = 0; m <= 2 * k; m++)
+            {
+                t = 0;
+                for (int p = 0; p < X.Length; p++)
+                    t += Math.Pow(X[p], m);
+
+                i = 0;
+                j = m;
+
+                while((i<=k)&&(j>=0))
+                {
+                    if (j <= k) C[i, j] = t;
+                    i++;
+                    j--;
+                }
+            }
+
+            // заполняем матрицу D
+            for (j = 0; j <= k; j++)
+            {
+                D[j] = 0;
+                for (i = 0; i < X.Length; i++)
+                    D[j] += Y[i] * Math.Pow(X[i], j);
+            }
+
+            // решаем систему методом Гаусса
+            double[] A = new double[k + 1];
+            int jop = gauss(k + 1, C, D, A);
+            return A;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            const int N = 5;
+            double[] X = new double[N];
+            double[] Y = new double[N];
+
+            X[0] = -2;
+            X[1] = -1;
+            X[2] = 0;
+            X[3] = 1;
+            X[4] = 2;
+
+            Y[0] = 3;
+            Y[1] = 0;
+            Y[2] = 10;
+            Y[3] = 0;
+            Y[4] = 3;
+
+
+            for (int i = 0; i < N; i++)
+                listBox1.Items.Add(X[i]);
+            for (int i = 0; i < N; i++)
+                listBox2.Items.Add(Y[i]);
+
+            for (int i = 0; i < N; i++)
+            { 
+                chart1.Series[1].Points.AddXY(X[i], Y[i]);
+            }
+
+            double[] A1 = naim_kvadrat(X, Y, 1);
+            double[] A2 = naim_kvadrat(X, Y, 2);
+            double[] A3 = naim_kvadrat(X, Y, 3);
+
+            for (double i = -2.1; i < 2.1; i += 0.1)
+            {
+                //double y = newton(X, Y, 0, i);
+                double y = lagrange(X, Y, i);
+                chart1.Series[0].Points.AddXY(i, y);
+                y = calc_polynom(A1, i);
+                chart1.Series[2].Points.AddXY(i, y);
+                y = calc_polynom(A2, i);
+                chart1.Series[3].Points.AddXY(i, y);
+                y = calc_polynom(A3, i);
+                chart1.Series[4].Points.AddXY(i, y);
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
